1fbe05ac75c55432bd64a8c222fe57f2ce73120d
yoshiaki <tesiyosi7211@gmail.com>
2026-04-01 10:48:10 +0900

    whisper専用の実行ファイル名に変更

--- main.py
+++
@@ -1,5 +0,0 @@
-import whisper # Whisperのインポート
-model = whisper.load_model('large') # 利用するモデルを指定（ここでは最も精度の高い'large'を指定）
-result = model.transcribe('音声データ.mp3') # resultに文字起こししたテキストデータを格納
-with open("書き起こしテキスト.txt", "w") as f:
-    print(result['text'], file=f) # テキストデータを書き出し
